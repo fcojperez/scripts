d389ec9c7bfb403989f7f7409a687f09b1a80af0
fcojperez <fcojperez@gmail.com>
2014-06-22 21:08:01 +0200

    Incluida la carpeta Synology para almacenar los scripts desarrollados para el NAS

---
+++ Synology/cvideo.sh
@@ -0,0 +1,8 @@
+# Script para convertir videos en un NAS Synology
+# es necesario tener instalado ffmpeg
+
+# Script for converting video in a Synology NAS
+# To run the script is needed to install fmpeg
+
+ffmpeg -i "$" -y -target pal-dvd "$2"
+
